ec988c7cf1bf4b149f7b8213512bf6a1450407bc
Jeffrey Pratt <@clementi1832>
2014-12-08 16:42:24 -0700

    Install htop and set port in dev environment

--- provision.sh
+++ provision.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+sudo apt-get install htop -y
 sudo apt-get install python-virtualenv -y
 sudo apt-get install python-dev -y
 sudo gem install foreman
 sudo virtualenv /home/vagrant/.ve/habcat-api-twisted
 sudo chown -R vagrant /home/vagrant/.ve
+sudo echo "export PORT=5000" >> /home/vagrant/.bashrc
