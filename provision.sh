d6d634c843d61520d65998afd9b4953e6d241d49
Jeffrey Pratt <@clementi1832>
2014-12-08 10:06:08 -0700

    Install python-dev

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get install python-virtualenv -y
+sudo apt-get install python-dev -y
 sudo gem install foreman
 sudo virtualenv /home/vagrant/.ve/habcat-api-twisted
 sudo chown -R vagrant /home/vagrant/.ve
