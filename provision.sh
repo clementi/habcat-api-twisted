7baad6bf98a46e49da2b84e69eab8f9c60dbdc3e
Jeffrey Pratt <@clementi1832>
2014-12-08 10:03:43 -0700

    Force install

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install python-virtualenv
+sudo apt-get install python-virtualenv -y
 sudo gem install foreman
 sudo virtualenv /home/vagrant/.ve/habcat-api-twisted
 sudo chown -R vagrant /home/vagrant/.ve
