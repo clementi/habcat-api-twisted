3e12cb91ff41e3d10dd36f3c7aad6f31bddfb114
Jeffrey Pratt <@clementi1832>
2014-12-08 10:14:21 -0700

    Add Twisted example

---
+++ main.py
@@ -0,0 +1,16 @@
+from twisted.web import server, resource
+from twisted.internet import reactor, endpoints
+
+
+class Counter(resource.Resource):
+    isLeaf = True
+    number_requests = 0
+
+    def render_GET(self, request):
+        self.number_requests += 1
+        request.setHeader('content-type', 'text/plain')
+        return "I am request #{}\n".format(self.number_requests)
+
+
+endpoints.serverFromString(reactor, "tcp:5000").listen(server.Site(Counter()))
+reactor.run()
