86d16b7eba20eaf234763db62c5b4db601750226
Jeff Pratt <me@jeffreypratt.net>
2014-12-09 23:04:53 -0700

    Get data from repo

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from twisted.enterprise import adbapi
 
 import json
 import os
+from repo import HabstarRepo
 
 
 data_file_path = './data/habcat.sqlite'
@@ -14,93 +15,16 @@ class Habstar(resource.Resource):
     isLeaf = True
 
     def render_GET(self, request):
-        action = (request.args.get('a') or ['browse'])[0]
-
-        page = int((request.args.get('p') or ['1'])[0])
-        page_size = 20
-        page_count = 857
-
-        request.setHeader('content-type', 'application/json')
-
-        if page <= 0 or page > page_count:
-            request.setResponseCode(500)
-            request.write(json.dumps({
-                'error_code': 'PAGE_NUM_OUT_OF_RANGE',
-                'message': 'The page number {} is out of range.'.format(page)
-            }))
-            request.finish()
-            return server.NOT_DONE_YET
-
-        def get_data():
-            if action == 'browse':
-                return dbpool.runQuery(
-                    'SELECT * FROM habstar ORDER BY hipparchos_num ASC LIMIT ? OFFSET ?;',
-                    (page_size, (page - 1) * page_size))
-            elif action == 'get':
-                hip_num = (request.args.get('hip') or ['6'])[0]
-                return dbpool.runQuery(
-                    'SELECT * FROM habstar WHERE hipparchos_num = ?', (hip_num))
-
-        def on_result(data):
-            if action == 'browse':
-                result = {
-                    'page': page,
-                    'total_pages': page_count,
-                    'habstars': []
-                }
-                for row in data:
-                    result['habstars'].append({
-                        'hip': row[0],
-                        'loc_cel': {
-                            'ra': [row[1], row[2], row[3]],
-                            'dec': [row[4], row[5], row[6]]
-                        },
-                        'loc_cart': [row[16], row[17], row[18]],
-                        'mag': row[7],
-                        'parx': {
-                            'val': row[8],
-                            'sigma': row[9]
-                        },
-                        'bmv': {
-                            'val': row[10],
-                            'sigma': row[11]
-                        },
-                        'ccdm': row[12],
-                        'hd': row[13],
-                        'bd': row[14],
-                        'dist': row[15]
-                    })
-            elif action == 'get':
-                result = {
-                    'hip': data[0][0],
-                        'loc_cel': {
-                            'ra': [data[0][1], data[0][2], data[0][3]],
-                            'dec': [data[0][4], data[0][5], data[0][6]]
-                        },
-                        'loc_cart': [data[0][16], data[0][17], data[0][18]],
-                        'mag': data[0][7],
-                        'parx': {
-                            'val': data[0][8],
-                            'sigma': data[0][9]
-                        },
-                        'bmv': {
-                            'val': data[0][10],
-                            'sigma': data[0][11]
-                        },
-                        'ccdm': data[0][12],
-                        'hd': data[0][13],
-                        'bd': data[0][14],
-                        'dist': data[0][15]
-                }
-
-            request.write(json.dumps(result))
+        def get_data(data):
+            request.setHeader('content-type', 'application/json')
+            request.write(json.dumps(data))
             request.finish()
 
-        d = get_data()
-        d.addCallback(on_result)
+        repo = HabstarRepo(dbpool)
+        d = repo.get_habstars_within_distance_to(6, 10)
+        d.addCallback(get_data)
 
         return server.NOT_DONE_YET
-
 
 port = os.environ.get('PORT', '80')
 
---
+++ repo.py
@@ -0,0 +1,39 @@
+class HabstarRepo(object):
+    def __init__(self, dbpool):
+        self._dbpool = dbpool
+
+    def get_habstar(self, hip_num):
+        query = 'SELECT * FROM habstar WHERE hipparchos_num = ?;'
+        return self._dbpool.runQuery(query, (hip_num,)).addCallback(self._build_habstar)
+
+    def _build_habstar(self, data):
+        row = data[0]
+        return {
+            'hip': row[0]
+        }
+
+    def get_habstars_within_distance_to(self, hip_num, distance):
+        distance_query = """SELECT * FROM habstar
+JOIN (SELECT x_pc AS ref_x, y_pc AS ref_y, z_pc AS ref_z FROM habstar WHERE hipparchos_num = ?)
+WHERE x_pc > ref_x - ? AND x_pc < ref_x + ?
+  AND y_pc > ref_y - ? AND y_pc < ref_y + ?
+  AND z_pc > ref_z - ? AND z_pc < ref_z + ?;"""
+        return self._dbpool.runQuery(
+            distance_query,
+            (hip_num, distance, distance, distance, distance, distance, distance)).addCallback(
+                lambda data: self._build_habstars_by_distance(data, distance))
+
+    def _build_habstars_by_distance(self, data, distance):
+        habstars = []
+        for row in data:
+            dist = self._distance(row[16], row[17], row[18], row[19], row[20], row[21])
+            if dist < distance:
+                habstars.append({
+                    'hip': row[0],
+                    'ref_dist': dist
+                })
+        return habstars
+
+    def _distance(self, x1, y1, z1, x2, y2, z2):
+        import math
+        return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
