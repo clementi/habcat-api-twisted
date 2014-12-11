b4813e3a054a0fc9a6f1c61397f73451134e7b26
Jeff Pratt <me@jeffreypratt.net>
2014-12-10 20:09:35 -0700

    Add method for getting habstars with similar color

--- repo.py
+++ repo.py
@@ -78,3 +78,37 @@ WHERE x_pc > ref_x - ? AND x_pc < ref_x + ?
     def _distance(self, x1, y1, z1, x2, y2, z2):
         import math
         return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+
+    def get_habstars_with_similar_color_to(self, color):
+        upper_color = color * 1.05
+        lower_color = color * 0.95
+        color_query = 'SELECT * FROM habstar WHERE b_minus_v < ? AND b_minus_v > ?'
+        return self._dbpool.runQuery(color_query, (upper_color, lower_color)).addCallback(self._build_habstars)
+
+    def _build_habstars(self, data):
+        habstars = []
+        for row in data:
+            hip_num, ra_hours, ra_minutes, ra_seconds, dec_degrees, dec_minutes, dec_seconds, johnson_mag, parx_mas, \
+                sigma_parx_mas, bmv, sigma_bmv, ccdm, hd, bd, dist_pc, x_pc, y_pc, z_pc = row
+            habstars.append({
+                'hip': hip_num,
+                'loc_cel': {
+                    'ra': [ra_hours, ra_minutes, ra_seconds],
+                    'dec': [dec_degrees, dec_minutes, dec_seconds]
+                },
+                'loc_cart_pc': [x_pc, y_pc, z_pc],
+                'mag': johnson_mag,
+                'parx_mas': {
+                    'v': parx_mas,
+                    's': sigma_parx_mas
+                },
+                'bmv': {
+                    'v': bmv,
+                    's': sigma_bmv
+                },
+                'ccdm': ccdm,
+                'hd': hd,
+                'bd': bd,
+                'dist_pc': dist_pc
+            })
+        return habstars
